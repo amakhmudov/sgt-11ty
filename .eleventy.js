67a0ce1054a94d76d2e3e2d916542a4c5237ecfb
Alisher Makhmudov <makhmudov.alisher@gmail.com>
2022-07-22 18:48:22 +0500

    sope changes

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,26 @@
+const htmlmin = require("html-minifier");
+
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('./src/css/main.css');
-  eleventyConfig.addPassthroughCopy('./src/img');
-  eleventyConfig.addPassthroughCopy('./src/js');
+  eleventyConfig.addPassthroughCopy("./src/css/main.css");
+  eleventyConfig.addPassthroughCopy("./src/img");
+  eleventyConfig.addPassthroughCopy("./src/js");
+  eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
+    if (outputPath && outputPath.endsWith(".html")) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      });
+      return minified;
+    }
+
+    return content;
+  });
+
   return {
     dir: {
-      input: 'src',
-      output: 'dest',
+      input: "src",
+      output: "dest",
     },
   };
 };
